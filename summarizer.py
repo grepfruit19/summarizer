8c546a83610ca630f95c43c3298d7c230f4f0873
WillKim <wtk219@nyu.edu>
2016-04-16 12:29:29 -0400

    Update summarizer.py

--- summarizer.py
+++ summarizer.py
@@ -29,3 +29,28 @@ with open(sys.argv[1], 'r') as input_file:
         i += 1
 
 
+#Everything below this line is stuff I've done, we can pick out what we want to keep later -Will
+
+#How will we attach scores to each sentence?
+#List of lists that are related to the layout of the original?
+
+paragraphlist = []
+
+for pindex, paragraph in enumerate(file):
+    for sindex, sentence in enumerate(paragraph):
+        paragraphlist[pindex][sindex]=rank(sentence)
+        if (sindex==0):
+            paragraphlist[pindex][sindex]+=5 #weights first sentence more heavily
+
+for qindex, qvector in enumerate(query_vectors):
+
+for paragraph in file:
+    paragraph[0]
+
+def rank(sentence):
+    output = 0
+    
+    return output
+
+def date_present(sentence):
+    
